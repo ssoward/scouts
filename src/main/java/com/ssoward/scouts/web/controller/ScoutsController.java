f87fdff1e72fd78b1ce158cbd46c4cc8b9348ffc
Clint Checketts <clintchecketts@ldschurch.org>
2014-01-14 13:27:52 -0700

    Add a log statement to debug weird security

--- src/main/java/com/ssoward/scouts/web/controller/ScoutsController.java
+++ src/main/java/com/ssoward/scouts/web/controller/ScoutsController.java
@@ -2,6 +2,7 @@ package com.ssoward.scouts.web.controller;
 
 
 import org.springframework.http.*;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,11 +21,13 @@ public class ScoutsController {
 
     @RequestMapping(method = RequestMethod.GET, value="/scouts", produces = MediaType.APPLICATION_JSON_VALUE)
     public String getscout(@RequestParam(value = "page", required = false, defaultValue = "0") int page) {
-        return "hello world, you must be a scout or at least have the right permissions."; //new ResponseEntity<>("hello scout", HttpStatus.OK);
+        System.out.println("scout:"+SecurityContextHolder.getContext().getAuthentication().getPrincipal());
+        return new ResponseEntity<>("hello scout", HttpStatus.OK);
     }
 
     @RequestMapping(method = RequestMethod.GET, value="/leaders", produces = MediaType.APPLICATION_JSON_VALUE)
     ResponseEntity<String> leader(@RequestParam(value = "page", required = false, defaultValue = "0") int page) {
+        System.out.println("leader:"+SecurityContextHolder.getContext().getAuthentication().getPrincipal());
         return new ResponseEntity<>("hello leader", HttpStatus.OK);
     }
 
